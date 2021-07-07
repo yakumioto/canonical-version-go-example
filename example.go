df6ce7b08bfde2f0130eaf8f0449ce44e5774c7b
yakumioto <yaku.mioto@gmail.com>
2021-07-07 16:07:30 +0800

    Wed Jul  7 16:07:30 CST 2021

--- example.go
+++ example.go
@@ -1,5 +1,12 @@
-package version
+package main
 
-func Version() string {
-	return "v1.1.3"
+import (
+	"fmt"
+	version "github.com/yakumioto/canonical-version-go-example"
+	version2 "github.com/yakumioto/canonical-version-go-example/v2"
+)
+
+func main() {
+	fmt.Println(version.Version())
+	fmt.Println(version2.Version())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module github.com/yakumioto/canonical-version-go-example
+module github.com/yakumioto/canonical-version-go
 
 go 1.16
+
+require (
+	github.com/yakumioto/canonical-version-go-example v1.1.3 // indirect
+	github.com/yakumioto/canonical-version-go-example/v2 v2.1.0 // indirect
+)
